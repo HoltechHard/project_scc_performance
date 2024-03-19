90387ec1d209c1d587a321dc9c96dda7afa7d6c5
Holger <hadronx@live.com>
2024-03-19 23:59:59 +0300

    updated inference engine with feature names

--- task01_scc/inference_controller.py
+++ task01_scc/inference_controller.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import pickle
+from typing import List
 from preprocess_controller import Preprocessor
 
 #       --- class for inference engine ---
@@ -10,9 +11,10 @@ class InferenceEngine:
     def __init__(self, path_model, path_db_categories):        
         self.model = self.load_model(path_model)
         self.preprocessor = Preprocessor(path_db_categories)
+        self.feature_names = self.preprocessor.db_categories["metadata"]
 
     # function to preprocess input data
-    def preprocessing(self, data: pd.DataFrame):
+    def prepare_df(self, data: pd.DataFrame) -> pd.DataFrame:
         # make preprocessing
         print("start preprocessing...")
         input, output = self.preprocessor.preprocessing_pipeline(data)
@@ -25,9 +27,13 @@ class InferenceEngine:
         with open(filename, "rb") as file:
             return pickle.load(file)
 
+    # function to get feature names
+    @property
+    def model_keys(self) -> List[str]:
+        return self.feature_names
+
     # function to predict output
-    def predict(self, data):        
-        x, _ = self.preprocessing(data)
+    def predict(self, x) -> pd.Series:
         print("generation of predictions...")
         y_pred = self.model.predict(x)
         return y_pred
